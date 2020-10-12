d24b6d09944668afd2723d28a8a9812e735c5f21
Matt Landreman <mattland@umd.edu>
2020-10-12 11:45:41 -0400

    Set up github actions to push to PyPI

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qsc",
+    version="0.0.1",
+    author="Matt Landreman",
+    author_email="matt.landreman@gmail.com",
+    description="Quasisymmetric Stellarator Construction",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/landreman/pyQSC",
+    packages=["qsc"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+#    packages=setuptools.find_packages(),
