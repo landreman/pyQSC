fd742a059f38931f28e1af012ef443dc453a5d9c
Matt Landreman <mattland@umd.edu>
2020-10-18 14:36:19 -0400

    Publish to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qsc",
-    version="0.0.2",
+    version="0.0.3",
     author="Matt Landreman",
     author_email="matt.landreman@gmail.com",
     description="Quasisymmetric Stellarator Construction",
