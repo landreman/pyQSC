a2a6222094e4a1b1e5a61cc63d7dd41c3289189c
Matt Landreman <mattland@umd.edu>
2022-01-11 08:50:31 -0500

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qsc",
-    version="0.0.5",
+    version="0.1.0",
     author="Matt Landreman",
     author_email="matt.landreman@gmail.com",
     description="Quasisymmetric Stellarator Construction",
