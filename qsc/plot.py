7986fc849b64b74036083a6e8241ab508267b8cd
Rogerio Jorge <rogeriojorge@Rogerios-MacBook-Pro.local>
2024-02-07 16:21:51 -0600

    changed default ntor mpol

--- qsc/plot.py
+++ qsc/plot.py
@@ -249,7 +249,7 @@ def create_subplot_mayavi(mlab, R, alphas, x_2D_plot, y_2D_plot, z_2D_plot,
         for j in range(len(alphas)):
             mlab.plot3d(fieldline_X_rotated[j], fieldline_Y_rotated[j]-shift_array[i], fieldline_Z_rotated[j], color=(0,0,0), line_width=0.001, tube_radius=0.005)
 
-def get_boundary(self, r=0.1, ntheta=40, nphi=130, ntheta_fourier=20, mpol=13, ntor=25, R_2D=None, Z_2D=None, output_fourier=False, lasym=None):
+def get_boundary(self, r=0.1, ntheta=40, nphi=130, ntheta_fourier=20, mpol=10, ntor=16, R_2D=None, Z_2D=None, output_fourier=False, lasym=None):
     '''
     Function that, for a given near-axis radial coordinate r, outputs
     the [X,Y,Z,R] components of the boundary. The resolution along the toroidal
